4a4d35cfd0e817bdf07d9d5c6c01118eea388a6f
MargheritaMottana <margherita.mottana@gmail.com>
2024-10-01 16:51:23 +0200

    scrittura lato php per prendere il numero scritto dall'utente: - impostato a null quando l'utente ancora non ha passato il dato; - intval per far sì che il dato passato dall'utente sia solo un numero; - considerato il numero passato dall'utente solo se compreso nel range >8 & <16;

--- index.php
+++ index.php
@@ -11,30 +11,55 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
+<?php
+    // imposto a null il valore di length, in questo modo, quando non è ancora stato passato un numero, non mi dà errore
+    $length = null
+
+    // controlo se mi viene passato (settato) il numero
+    if(isset$_GET['pwLength']){
+
+        // intval mi serve per avere "davvero" un numero (come parseInt)
+        $length = intval($_GET['pwLength']);
+
+        // considero il numero solo se entra in questo range >8 e <16
+        if($length >= 8 && $length <= 16){
+            
+        }
+    }
+
+?>
+
 <body>
 
     <main class="container py-5 text-center">
-        <h1 class="mb-4">Genera la tua password</h1>
+        <div class="row justify-content-center">
+            <div class="col-6">
 
-        <form action="" method="GET">
+                <h1 class="mb-4 text-primary-emphasis">Genera la tua password</h1>
 
-            <div class="mb-3">
-                <label for="inputNumber" class="form-label fs-4">Scegli la lunghezza della tua password</label>
+                <hr class="mb-5">
 
-                <input class="form-control" id="inputNumber" 
-                placeholder="Scrivi un numero da 8 a 16"
-                type="number"  
-                required 
-                min=8 
-                max=16
-                name="pwLength">
+                <form action="" method="GET">
+
+                    <div class="mb-3">
+                        <label for="inputNumber" class="form-label fs-5 mb-3 text-primary">Scegli la lunghezza della tua password</label>
+
+                        <input class="form-control mb-4" id="inputNumber" 
+                        placeholder="Scrivi un numero da 8 a 16"
+                        type="number"  
+                        required 
+                        min="8" 
+                        max="16"
+                        name="pwLength">
+
+                    </div>
+
+                    <button type="submit" class="btn btn-outline-primary">Invia</button>
+
+                </form>
 
             </div>
-
-            <button type="submit" class="btn btn-primary">Invia</button>
-
-        </form>
-
+        </div>
     </main>
 
     <!-- bs -->
